9b3a45450756c871f37e5051b1b266cc1d86edbd
Josh Justice <me@codingitwrong.com>
2019-01-06 15:06:27 -0600

    Add prettier

---
+++ template/.eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  globals: {
+    server: true,
+  },
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  plugins: [
+    'ember'
+  ],
+  extends: [
+    'plugin:ember/recommended',
+    'codingitwrong',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    browser: true
+  },
+  rules: {
+    "camelcase": "off", // for destructuring underscored values
+    "require-await": "off", // for consistency in rendering tests
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        '.eslintrc.js',
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'testem.js',
+        'ember-cli-build.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'lib/*/index.js'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      }
+    }
+  ]
+};
