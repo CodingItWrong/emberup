afaebdc99f4810859d5abcade4c4cf0cc9c44c9c
Josh Justice <me@codingitwrong.com>
2021-05-27 07:41:32 -0400

    Remove computed from app adapter

--- template/app/adapters/application.js
+++ template/app/adapters/application.js
@@ -1,5 +1,4 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
-import { computed } from '@ember/object';
 // eslint-disable-next-line ember/no-mixins
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import ENV from '../config/environment';
@@ -9,7 +8,6 @@ export default class ApplicationAdapter extends JSONAPIAdapter.extend(
 ) {
   host = ENV.apiHost;
 
-  @computed('session.{isAuthenticated,data.authenticated.access_token}')
   get headers() {
     let headers = {};
 
